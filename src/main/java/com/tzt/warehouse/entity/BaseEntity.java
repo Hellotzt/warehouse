9c568dce45fd01701b56efc17a150d2119d4c8eb
Tangzt <tzt123456>
2022-09-24 02:09:31 +0800

    时间戳转换

--- src/main/java/com/tzt/warehouse/entity/BaseEntity.java
+++ src/main/java/com/tzt/warehouse/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.tzt.warehouse.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -21,16 +22,19 @@ public class BaseEntity {
      * 创建时间
      */
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
     /**
      * 更新人
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+
     private String updateBy;
     /**
      * 更新时间
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
     /**
      * 删除标志（0代表未删除，1代表已删除）
